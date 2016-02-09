214f0a2adfc8c57c052c706638785809a024f940
Paul Chesnais <paul.chesnais@gmail.com>
2016-02-09 17:59:56 -0500

    Untracked dropbox ssh

--- dropbox-ssh/dbssh/main.py
+++
@@ -1,8 +0,0 @@
-from socket import gethostname
-from os.path import expanduser, join
-from os import environ
-
-hostname = gethostname()
-dropbox_home = environ["DROPBOX_HOME"] if environ.has_key("DROPBOX_HOME") else join(expanduser("~"), "Dropbox")
-dbssh_home = join(dropbox_home, "dbssh", hostname)
-
